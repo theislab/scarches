824e88d16501eefad0c52b5ef397a60275e25f91
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-08-13 13:20:14 +0200

    Add some utility functions

--- surgeon/models/_utils.py
+++ surgeon/models/_utils.py
@@ -77,3 +77,22 @@ def sample_z(args):
     z_dim = K.int_shape(mu)[1]
     eps = K.random_normal(shape=[batch_size, z_dim])
     return mu + K.exp(log_var / 2) * eps
+
+
+def _nan2zero(x):
+    return tf.where(tf.is_nan(x), tf.zeros_like(x), x)
+
+
+def _nan2inf(x):
+    return tf.where(tf.is_nan(x), tf.zeros_like(x) + np.inf, x)
+
+
+def _nelem(x):
+    nelem = tf.reduce_sum(tf.cast(~tf.is_nan(x), tf.float32))
+    return tf.cast(tf.where(tf.equal(nelem, 0.), 1., nelem), x.dtype)
+
+
+def _reduce_mean(x):
+    nelem = _nelem(x)
+    x = _nan2zero(x)
+    return tf.divide(tf.reduce_sum(x), nelem)
