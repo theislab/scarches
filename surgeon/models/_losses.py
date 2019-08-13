bf1af3a7ef401317b4cb73f05d62e77a7c7c535f
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-08-13 13:20:25 +0200

    Add NB, ZINB losses

--- surgeon/models/_losses.py
+++ surgeon/models/_losses.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from keras import backend as K
+import numpy as np
 
-from surgeon.models._utils import compute_mmd
+from ._utils import compute_mmd, _nelem, _nan2zero, _nan2inf, _reduce_mean
 
 
 def kl_recon(mu, log_var, alpha=0.1, eta=1.0):
@@ -25,3 +26,90 @@ def mmd(n_conditions, beta, kernel_method='multi-scale-rbf'):
             return beta * loss
 
     return mmd_loss
+
+
+class NB(object):
+    def __init__(self, theta=None, masking=False, scope='nbinom_loss/',
+                 scale_factor=1.0):
+
+        # for numerical stability
+        self.eps = 1e-10
+        self.scale_factor = scale_factor
+        self.scope = scope
+        self.masking = masking
+        self.theta = theta
+
+    def loss(self, y_true, y_pred, mean=True):
+        scale_factor = self.scale_factor
+        eps = self.eps
+
+        with tf.name_scope(self.scope):
+            y_true = tf.cast(y_true, tf.float32)
+            y_pred = tf.cast(y_pred, tf.float32) * scale_factor
+
+            if self.masking:
+                nelem = _nelem(y_true)
+                y_true = _nan2zero(y_true)
+
+            # Clip theta
+            theta = tf.minimum(self.theta, 1e6)
+
+            t1 = tf.lgamma(theta + eps) + tf.lgamma(y_true + 1.0) - tf.lgamma(y_true + theta + eps)
+            t2 = (theta + y_true) * tf.log(1.0 + (y_pred / (theta + eps))) + (
+                    y_true * (tf.log(theta + eps) - tf.log(y_pred + eps)))
+            final = t1 + t2
+
+            final = _nan2inf(final)
+
+            if mean:
+                if self.masking:
+                    final = tf.divide(tf.reduce_sum(final), nelem)
+                else:
+                    final = tf.reduce_mean(final)
+
+        return final
+
+
+class ZINB(NB):
+    def __init__(self, pi, ridge_lambda=0.0, scope='zinb_loss/', **kwargs):
+        super().__init__(scope=scope, **kwargs)
+        self.pi = pi
+        self.ridge_lambda = ridge_lambda
+
+    def loss(self, y_true, y_pred, mean=True):
+        scale_factor = self.scale_factor
+        eps = self.eps
+
+        with tf.name_scope(self.scope):
+            # reuse existing NB neg.log.lik.
+            # mean is always False here, because everything is calculated
+            # element-wise. we take the mean only in the end
+            nb_case = super().loss(y_true, y_pred, mean=False) - tf.log(1.0 - self.pi + eps)
+
+            y_true = tf.cast(y_true, tf.float32)
+            y_pred = tf.cast(y_pred, tf.float32) * scale_factor
+            theta = tf.minimum(self.theta, 1e6)
+
+            zero_nb = tf.pow(theta / (theta + y_pred + eps), theta)
+            zero_case = -tf.log(self.pi + ((1.0 - self.pi) * zero_nb) + eps)
+            result = tf.where(tf.less(y_true, 1e-8), zero_case, nb_case)
+            ridge = self.ridge_lambda * tf.square(self.pi)
+            result += ridge
+
+            if mean:
+                if self.masking:
+                    result = _reduce_mean(result)
+                else:
+                    result = tf.reduce_mean(result)
+
+            result = _nan2inf(result)
+
+        return result
+
+
+LOSSES = {
+    "mse": kl_recon,
+    "mmd": mmd,
+    "nb": NB,
+    "zinb": ZINB,
+}
