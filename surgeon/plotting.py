241178f3f29d32928c94cc3c04b3982964f791ba
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-08-20 14:13:45 +0200

    remove plotting functions

--- surgeon/plotting.py
+++
@@ -1,9 +0,0 @@
-from surgeon.models import CVAE
-
-
-def plot_runtimes(time: tuple):
-    for before_net, after_net in network_tuples:
-        before_time = before_net.run_time
-        after_time = after_net.run_time
-
-
