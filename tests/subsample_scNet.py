8e9500ccb88d57fa73a4fc375c8529e064ada603
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-09-02 14:29:31 +0200

    update subsample test for scNet with new subsample indices

--- tests/subsample_scNet.py
+++ tests/subsample_scNet.py
@@ -7,7 +7,8 @@ import scanpy as sc
 import surgeon
 
 DATASETS = {
-    "pancreas": {"name": "pancreas", "batch_key": "study", "cell_type_key": "cell_type", "target": ["Pancreas Celseq", "Pancreas CelSeq2"]},
+    "pancreas": {"name": "pancreas", "batch_key": "study", "cell_type_key": "cell_type",
+                 "target": ["Pancreas Celseq", "Pancreas CelSeq2"]},
     "toy": {"name": "pancreas", "batch_key": "batch", "cell_type_key": "celltype", "target": ["Batch8", "Batch9"]},
 }
 
@@ -51,90 +52,94 @@ def train_and_evaluate(data_dict, freeze=True, count_adata=True):
     else:
         clip_value = 1e6
 
-    scores = []
-    for subsample_frac in [1.0, 0.8, 0.6, 0.4, 0.2, 0.1]:
-        train_adata, valid_adata = surgeon.utils.train_test_split(adata_for_training, 0.85)
-        n_conditions = len(train_adata.obs[condition_key].unique().tolist())
+    for i in range(5):
+        scores = []
+        for subsample_frac in [1.0, 0.8, 0.6, 0.4, 0.2, 0.1]:
+            adata_out_of_sample_subsampled = None
+            for condition in target_conditions:
+                condition_adata = adata_out_of_sample[adata_out_of_sample.obs[condition_key] == condition]
+                keep_idx = np.loadtxt(f'./data/subsample/{data_name}/{condition}/{subsample_frac}/{i}.csv',
+                                      dtype='int32')
+                condition_adata_subsampled = condition_adata[keep_idx, :]
 
-        network = surgeon.archs.CVAE(x_dimension=train_adata.shape[1],
-                                     z_dimension=20,
-                                     n_conditions=n_conditions,
-                                     lr=0.001,
-                                     alpha=0.001,
-                                     eta=1.0,
-                                     clip_value=clip_value,
-                                     loss_fn=loss_fn,
-                                     model_path=f"./models/CVAE/Subsample/before-{data_name}-{loss_fn}/",
-                                     dropout_rate=0.2,
-                                     output_activation='relu')
+                adata_out_of_sample_subsampled = condition_adata_subsampled if adata_out_of_sample is None \
+                    else adata_out_of_sample_subsampled.concatenate(condition_adata_subsampled)
 
-        conditions = adata_for_training.obs[condition_key].unique().tolist()
-        condition_encoder = surgeon.utils.create_dictionary(conditions, target_conditions)
+            train_adata, valid_adata = surgeon.utils.train_test_split(adata_for_training, 0.85)
+            n_conditions = len(train_adata.obs[condition_key].unique().tolist())
 
-        network.train(train_adata,
-                      valid_adata,
-                      condition_key=condition_key,
-                      cell_type_key=cell_type_key,
-                      le=condition_encoder,
-                      n_epochs=10000,
-                      batch_size=32,
-                      early_stop_limit=20,
-                      lr_reducer=15,
-                      n_per_epoch=0,
-                      save=True,
-                      retrain=False,
-                      verbose=2)
+            network = surgeon.archs.CVAE(x_dimension=train_adata.shape[1],
+                                         z_dimension=20,
+                                         n_conditions=n_conditions,
+                                         lr=0.001,
+                                         alpha=0.001,
+                                         eta=1.0,
+                                         clip_value=clip_value,
+                                         loss_fn=loss_fn,
+                                         model_path=f"./models/CVAE/Subsample/before-{data_name}-{loss_fn}/",
+                                         dropout_rate=0.2,
+                                         output_activation='relu')
 
-        new_network = surgeon.operate(network,
-                                      new_conditions=target_conditions,
-                                      init='Xavier',
-                                      freeze=freeze)
-        
-        new_network.model_path = f"./models/CVAE/Subsample/after-{data_name}-{loss_fn}-{subsample_frac}/"
+            conditions = adata_for_training.obs[condition_key].unique().tolist()
+            condition_encoder = surgeon.utils.create_dictionary(conditions, target_conditions)
 
-        if subsample_frac < 1.0:
-            keep_idx = np.loadtxt(f'./data/subsample/pancreas_N*{subsample_frac}.csv')
-            keep_idx = keep_idx.astype('int32')
-        else:
-            n_samples = adata_out_of_sample.shape[0]
-            keep_idx = np.random.choice(n_samples, n_samples, replace=False)
-        
-        adata_out_of_sample_subsampled = adata_out_of_sample[keep_idx, :]
-        train_adata, valid_adata = surgeon.utils.train_test_split(adata_out_of_sample_subsampled, 0.85)
-
-        new_network.train(train_adata,
+            network.train(train_adata,
                           valid_adata,
                           condition_key=condition_key,
                           cell_type_key=cell_type_key,
-                          le=new_network.condition_encoder,
-                          n_epochs=300,
+                          le=condition_encoder,
+                          n_epochs=10000,
                           batch_size=32,
-                          early_stop_limit=25,
-                          lr_reducer=20,
+                          early_stop_limit=20,
+                          lr_reducer=15,
                           n_per_epoch=0,
                           save=True,
+                          retrain=False,
                           verbose=2)
 
-        encoder_labels, _ = surgeon.utils.label_encoder(
-            adata_out_of_sample_subsampled, label_encoder=network.condition_encoder, condition_key=condition_key)
+            new_network = surgeon.operate(network,
+                                          new_conditions=target_conditions,
+                                          init='Xavier',
+                                          freeze=freeze)
 
-        latent_adata = new_network.to_latent(adata_out_of_sample_subsampled, encoder_labels)
-        
-        ebm = surgeon.metrics.entropy_batch_mixing(latent_adata, label_key=condition_key, n_pools=1)
-        asw = surgeon.metrics.asw(latent_adata, label_key=condition_key)
-        ari = surgeon.metrics.ari(latent_adata, label_key=cell_type_key)
-        nmi = surgeon.metrics.nmi(latent_adata, label_key=cell_type_key)
+            new_network.model_path = f"./models/CVAE/subsample/after-{data_name}-{loss_fn}-{subsample_frac}-{freeze}/"
 
-        scores.append([subsample_frac, ebm, asw, ari, nmi])
-        print([subsample_frac, ebm, asw, ari, nmi])
+            train_adata, valid_adata = surgeon.utils.train_test_split(adata_out_of_sample_subsampled, 0.85)
 
-    scores = np.array(scores)
+            new_network.train(train_adata,
+                              valid_adata,
+                              condition_key=condition_key,
+                              cell_type_key=cell_type_key,
+                              le=new_network.condition_encoder,
+                              n_epochs=300,
+                              batch_size=32,
+                              early_stop_limit=40,
+                              lr_reducer=35,
+                              n_per_epoch=0,
+                              save=True,
+                              verbose=2)
 
-    filename = "scores_scNet"
-    filename += "Freezed" if freeze else "UnFreezed"
-    filename += "_count.log" if count_adata else "_normalized.log"
+            encoder_labels, _ = surgeon.utils.label_encoder(
+                adata_out_of_sample_subsampled, label_encoder=network.condition_encoder, condition_key=condition_key)
 
-    np.savetxt(os.path.join(path_to_save, filename), X=scores, delimiter=",")
+            latent_adata = new_network.to_latent(adata_out_of_sample_subsampled, encoder_labels)
+
+            ebm = surgeon.metrics.entropy_batch_mixing(latent_adata, label_key=condition_key, n_pools=1)
+            asw = surgeon.metrics.asw(latent_adata, label_key=condition_key)
+            ari = surgeon.metrics.ari(latent_adata, label_key=cell_type_key)
+            nmi = surgeon.metrics.nmi(latent_adata, label_key=cell_type_key)
+
+            scores.append([subsample_frac, ebm, asw, ari, nmi])
+            print([subsample_frac, ebm, asw, ari, nmi])
+
+        scores = np.array(scores)
+
+        filename = "scores_scNet"
+        filename += "Freezed" if freeze else "UnFreezed"
+        filename += "_count" if count_adata else "_normalized"
+        filename += f"_{i}.log"
+
+        np.savetxt(os.path.join(path_to_save, filename), X=scores, delimiter=",")
 
 
 if __name__ == '__main__':
