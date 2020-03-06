d405cc38278ba416594cf0b404294b406a05b7e2
naghipourfar <mn7697np@gmail.com>
2020-03-06 07:32:39 +0000

    subsample script for scVI updated

--- tests/subsample_scVI.py
+++ tests/subsample_scVI.py
@@ -1,13 +1,12 @@
 import sys
 
-import surgeon
-
 sys.path.append("../")
 
 import scanpy as sc
 from sklearn.preprocessing import LabelEncoder
 from _scVI import ADataset, scVI_Trainer
 from scvi.models.vae import VAE
+from scvi.dataset import AnnDatasetFromAnnData
 import numpy as np
 import torch
 import os
@@ -42,15 +41,15 @@ adata = sc.read(f"./data/{data_name}/{data_name}_normalized.h5ad")
 adata.X = adata.raw.X
 adata = remove_sparsity(adata)
 
-n_epochs = 300
+n_epochs = 1000
 lr = 1e-3
 early_stopping_kwargs = {
     "early_stopping_metric": "elbo",
     "save_best_state_metric": "elbo",
-    "patience": 50,
+    "patience": 10,
     "threshold": 0,
     "reduce_lr_on_plateau": True,
-    "lr_patience": 40,
+    "lr_patience": 8,
     "lr_factor": 0.1,
 }
 use_batches = True
@@ -72,11 +71,11 @@ for i in range(5):
         final_adata.obs['labels'] = LabelEncoder().fit_transform(final_adata.obs[cell_type_key])
         final_adata.obs['batch_indices'] = LabelEncoder().fit_transform(final_adata.obs[batch_key])
 
-        scvi_dataset = ADataset(final_adata)
+        scvi_dataset = AnnDatasetFromAnnData(final_adata)
 
         print(scvi_dataset.n_batches, scvi_dataset.n_labels)
 
-        vae = VAE(scvi_dataset.nb_genes, n_batch=scvi_dataset.n_batches * use_batches)
+        vae = VAE(scvi_dataset.nb_genes, n_batch=scvi_dataset.n_batches * use_batches, n_layers=2, n_hidden=128)
 
         model = scVI_Trainer(vae, scvi_dataset,
                              train_size=0.80,
