2b6d2bc9d8b6c61d5a48c8ada032bd177015f916
naghipourfar <mn7697np@gmail.com>
2020-04-10 13:15:03 +0000

    Toy data generator updated

--- Toy.R
+++ Toy.R
@@ -2,18 +2,18 @@ library(splatter)
 library(scater)
 
 params <- newSplatParams()
-params <- setParams(params, update = list(nGenes = 10000))
+params <- setParams(params, update = list(nGenes = 5000))
 
 params <- setParams(params, update = list(batchCells = c(4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000)))
-params <- setParams(params, update = list(batch.facLoc = c(0.5)))
-params <- setParams(params, update = list(batch.facScale = c(0.5)))
+params <- setParams(params, update = list(batch.facLoc = 0.5))
+params <- setParams(params, update = list(batch.facScale = 2.0))
                     
 params <- setParams(params, update = list(group.prob = c(0.2, 0.2, 0.2, 0.2, 0.2)))
 
 sim <- splatSimulate(params, verbose = FALSE, method = "groups")
 sim <- normalize(sim)
 
-plotPCA(sim, colour_by = "Group")
+# plotPCA(sim, colour_by = "Group")
 
 write.table(x = counts(sim), file = "./data/toy/toy.csv", sep = ",")
 write.table(x = sim@colData@listData[["Batch"]], file = "./data/toy/toy_batch.csv", sep = ',')
