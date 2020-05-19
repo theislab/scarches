f1d8b2eeb8d7a787cfb98009d9543fd2e0a38258
naghipourfar <mohsen.naghipourfar@gmail.com>
2020-05-19 18:38:01 +0430

    upload_model wrapper to zenodo added

---
+++ scnet/zenodo/__init__.py
@@ -0,0 +1,17 @@
+from scnet.models import scNet
+from scnet.zenodo.file import upload_file
+from scnet.zenodo.zip import zip_model_directory
+
+
+def upload_model(model: scNet,
+                 deposition_id: str,
+                 access_token: str):
+    model_path = model.model_path
+    output_base_name = f"/tmp/scNet-{model.task_name}"
+    output_path = output_base_name + ".zip"
+    zip_model_directory(output_path=output_base_name, directory=model_path)
+    download_link = upload_file(file_path=output_path,
+                                deposition_id=deposition_id,
+                                access_token=access_token)
+    print("Model has been successfully uploaded")
+    return download_link
