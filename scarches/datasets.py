b082238112bf0ed7c5a79b1137e0de5097ca689f
naghipourfar <mn7697np@gmail.com>
2020-07-06 00:39:59 +0430

    doc page added for datasets module

--- scarches/datasets.py
+++ scarches/datasets.py
@@ -6,6 +6,6 @@ def pancreas():
     Automatically downloads 5 pancreatic normalized datasets with 1000 selected highly variable genes.
     """
     url = f"https://zenodo.org/record/3930949/files/pancreas_normalized.h5ad?download=1"
-    adata = sc.read("./data/", backup_url=url, sparse=True, cache=True)
+    adata = sc.read("./data/pancreas_normalized_hvg.h5ad", backup_url=url, sparse=True, cache=True)
     adata.var_names_make_unique()
     return adata
