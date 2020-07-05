d03be24d2a1b8d5e1ebf6b48fff28e9c2d2a435b
naghipourfar <mn7697np@gmail.com>
2020-07-05 17:37:37 +0430

    test script updated

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,30 +2,32 @@ import scarches as sca
 import numpy as np
 import scanpy as sc
 
-# reference adata
-X = np.random.normal(size=(1000, 10))
-adata = sc.AnnData(X=X)
-adata.obs['cell_type'] = list("ABCDE") * 200
-adata.obs['condition'] = list("MNOP") * 250
-adata.raw = adata
-adata.obs['size_factors'] = 1.0
 
-# print(len(adata.obs['condition'].unique().tolist()))
-# print(len(adata.obs['cell_type'].unique().tolist()))
+def test_api():
+    # reference adata
+    X = np.random.normal(size=(1000, 10))
+    adata = sc.AnnData(X=X)
+    adata.obs['cell_type'] = list("ABCDE") * 200
+    adata.obs['condition'] = list("MNOP") * 250
+    adata.raw = adata
+    adata.obs['size_factors'] = 1.0
 
-model = sca.models.scArches(10, list('MNOP'))
-model.train(adata, "condition", 0.8)
+    # print(len(adata.obs['condition'].unique().tolist()))
+    # print(len(adata.obs['cell_type'].unique().tolist()))
 
-print(model.network_kwargs)
-# query adata
-X = np.random.normal(size=(1000, 10))
-adata = sc.AnnData(X=X)
-adata.obs['cell_type'] = list("ABCL") * 250
-adata.obs['condition'] = list("QRST") * 250
-adata.raw = adata
-adata.obs['size_factors'] = 1.0
+    model = sca.models.scArches(10, list('MNOP'))
+    model.train(adata, "condition", 0.8)
 
-new_model = sca.operate(model, "new_task", adata.obs['condition'].unique().tolist())
-print(new_model.network_kwargs)
-# print(new_model.n_conditions, new_model.n_mmd_conditions, new_model.condition_encoder)
-new_model.train(adata, "condition", 0.8, n_epochs=1)
+    print(model.network_kwargs)
+    # query adata
+    X = np.random.normal(size=(1000, 10))
+    adata = sc.AnnData(X=X)
+    adata.obs['cell_type'] = list("ABCL") * 250
+    adata.obs['condition'] = list("QRST") * 250
+    adata.raw = adata
+    adata.obs['size_factors'] = 1.0
+
+    new_model = sca.operate(model, "new_task", adata.obs['condition'].unique().tolist())
+    print(new_model.network_kwargs)
+    # print(new_model.n_conditions, new_model.n_mmd_conditions, new_model.condition_encoder)
+    new_model.train(adata, "condition", 0.8, n_epochs=1)
