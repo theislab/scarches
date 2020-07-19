d65d50dd40c5ac0a8dc6e75ebeb03d021efda574
naghipourfar <mn7697np@gmail.com>
2020-07-19 11:50:08 +0430

    scANet removed

--- scarches/models/__init__.py
+++ scarches/models/__init__.py
@@ -2,8 +2,7 @@ from .cvae import CVAE
 from .scarches import scArches
 from .scarchesnb import scArchesNB
 from .scarcheszinb import scArchesZINB
-from .scanet import scANet
 
 __all__ = (
-    'scArches', 'CVAE', 'scANet', 'scArchesNB', 'scArchesZINB'
+    'scArches', 'CVAE', 'scArchesNB', 'scArchesZINB'
 )
