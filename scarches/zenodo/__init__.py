d004f7561d78daaf5cd14edb111ae6b27cae28d5
naghipourfar <mn7697np@gmail.com>
2020-07-23 17:36:19 +0430

    upload functions for adaptor added

--- scarches/zenodo/__init__.py
+++ scarches/zenodo/__init__.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from scarches import Adaptor
 from scarches.models import scArches, scArchesZINB, scArchesNB, CVAE
 from .file import *
 from .deposition import *
@@ -34,3 +35,59 @@ def upload_model(model: Union[scArches, CVAE, scArchesNB, scArchesZINB],
                                 access_token=access_token)
     print("Model has been successfully uploaded")
     return download_link
+
+
+def upload_adaptor(adaptor: Adaptor,
+                   deposition_id: str,
+                   access_token: str):
+    """uploads trained ``model`` to Zenodo.
+
+        Parameters
+        ----------
+        adaptor: :class:`~scarches.Adaptor`
+            An instance of Adaptor class.
+        deposition_id: str
+            ID of a deposition in your Zenodo account.
+        access_token: str
+            Your Zenodo Access token.
+
+        Returns
+        -------
+        download_link: str
+            Generated direct download link for the uploaded model in the deposition. Please **Note** that the link is usable **after** your published your deposition.
+    """
+    import pickle
+    output_base_name = f"/tmp/adaptor-{adaptor.study}"
+    output_path = output_base_name + ".pkl"
+    pickle.dump(adaptor, open(output_path, 'wb'))
+    download_link = upload_file(file_path=output_path,
+                                deposition_id=deposition_id,
+                                access_token=access_token)
+    print(f"Adaptor-{adaptor.study} has been successfully uploaded")
+    return download_link
+
+
+def upload_adaptors(adaptors: list,
+                    deposition_id: str,
+                    access_token: str):
+    """uploads trained ``model`` to Zenodo.
+
+        Parameters
+        ----------
+        adaptors: list of :class:`~scarches.Adaptor`
+            Python's list of Adaptor's objects.
+        deposition_id: str
+            ID of a deposition in your Zenodo account.
+        access_token: str
+            Your Zenodo Access token.
+
+        Returns
+        -------
+        download_link: str
+            Generated direct download link for the uploaded model in the deposition. Please **Note** that the link is usable **after** your published your deposition.
+    """
+    download_links = {}
+    for adaptor in adaptors:
+        download_links[adaptor.study] = upload_adaptor(adaptor, deposition_id, access_token)
+
+    return download_links
