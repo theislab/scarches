34efaeef0812cfeb39c3c1453b56e0453f42eef9
naghipourfar <mohsen.naghipourfar@gmail.com>
2020-09-13 12:29:26 +0430

    docs conf and requirements updated

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 
 def linkcode_resolve(domain, info):
