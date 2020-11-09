fixed ZINB loss computation bug, get_latent, and model_path

scarches/models/cvae.py
@@ -130,11 +130,10 @@ class CVAE(Model):
         }
 
         self.training_kwargs = {
-            "learning_rate": self.lr,
+            "lr": self.lr,
             "alpha": self.alpha,
             "eta": self.eta,
             "clip_value": self.clip_value,
-            "model_path": self.model_base_path,
         }
 
         self.init_w = keras.initializers.glorot_normal()
@@ -169,11 +168,10 @@ class CVAE(Model):
         }
 
         self.training_kwargs = {
-            "learning_rate": self.lr,
+            "lr": self.lr,
             "alpha": self.alpha,
             "eta": self.eta,
             "clip_value": self.clip_value,
-            "model_path": self.model_base_path,
         }
 
     @classmethod
@@ -422,9 +420,9 @@ class CVAE(Model):
 
         encoder_inputs = [adata.X, encoder_labels]
         if return_mean:
-            latent = self.encoder_model.predict(encoder_inputs)[0]
+            latent = self.encoder.predict(encoder_inputs)[0]
         else:
-            latent = self.encoder_model.predict(encoder_inputs)[2]
+            latent = self.encoder.predict(encoder_inputs)[2]
 
         latent = np.nan_to_num(latent, nan=0.0, posinf=0.0, neginf=0.0)
 

scarches/models/scarcheszinb.py
@@ -165,7 +165,7 @@ class scArchesZINB(CVAE):
 
         # Building the model via calling it with a random input
         input_arr = [tf.random.uniform((1, self.x_dim)), tf.ones((1, self.n_conditions)),
-                     tf.ones((1, self.n_conditions)), tf.ones(1, 1, dtype=tf.float32)]
+                     tf.ones((1, self.n_conditions)), tf.ones(1, dtype=tf.float32)]
         self(input_arr)
 
         get_custom_objects().update(self.custom_objects)
@@ -185,6 +185,8 @@ class scArchesZINB(CVAE):
 
     def forward_with_loss(self, data):
         x, y = data
+    
+        y = y['reconstruction']    
         y_pred, z_mean, z_log_var, disp, pi = self.call(x)
         loss, recon_loss, kl_loss = self.calc_losses(y, y_pred, z_mean, z_log_var, disp, pi)
 