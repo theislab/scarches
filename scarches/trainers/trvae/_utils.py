65ef0408829e14758614554bf51f2d1e8d83a0b4
marco <marco-wagenstetter@t-online.de>
2021-05-18 14:31:50 +0200

    Removed euclidean distance from utils, added on_epoch_gein placeholder

--- scarches/trainers/trvae/_utils.py
+++ scarches/trainers/trvae/_utils.py
@@ -192,21 +192,3 @@ def custom_collate(batch):
     elif isinstance(elem, container_abcs.Mapping):
         output = {key: custom_collate([d[key] for d in batch]) for key in elem}
         return output
-
-
-def euclidean_dist(x, y):
-    """
-    Compute euclidean distance between two tensors
-    """
-    # x: N x D
-    # y: M x D
-    n = x.size(0)
-    m = y.size(0)
-    d = x.size(1)
-    if d != y.size(1):
-        raise Exception
-
-    x = x.unsqueeze(1).expand(n, m, d)
-    y = y.unsqueeze(0).expand(n, m, d)
-
-    return torch.pow(x - y, 2).sum(2)
