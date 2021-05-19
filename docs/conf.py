d85742b632128bd9dcba52729a9909cdff4a97fa
M0hammadL <33202701+M0hammadL@users.noreply.github.com>
2021-05-19 10:02:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ html_static_path = ['_static']
 
 
 def setup(app):
-    app.add_css_file()('custom.css')
+    app.add_css_file('custom.css')
 
 # -- General configuration ---------------------------------------------------
 
