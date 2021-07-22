7dc4e860f4b6d77d361a89e8ffa85c65d6cfdd0b
Lieke Michielsen <42767830+lcmmichielsen@users.noreply.github.com>
2021-07-22 15:51:01 +0200

    Update __init__.py

--- scarches/classifiers/__init__.py
+++ scarches/classifiers/__init__.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 19 10:12:28 2021
-
-@author: lcmmichielsen
-"""
-
 from scHPL.train import train_tree
 from scHPL.learn import learn_tree
 from scHPL.predict import predict_labels
