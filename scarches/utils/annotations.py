32708b402f9f9e4c4c601b5b1ff67ba143c31a5b
Koncopd <koncopd@gmail.com>
2021-07-27 18:22:28 +0200

    add clean option to annotations

--- scarches/utils/annotations.py
+++ scarches/utils/annotations.py
@@ -2,20 +2,21 @@ import numpy as np
 
 # add binary I of size n_vars x number of annotated terms in files
 # if I[i,j]=1 then gene i is active in annotation j
-def add_annotations(adata, files, min_genes=0, max_genes=None, varm_key='I', uns_key='terms'):
+def add_annotations(adata, files, min_genes=0, max_genes=None, varm_key='I', uns_key='terms',
+                    clean=True):
     files = [files] if isinstance(files, str) else files
     annot = []
 
     for file in files:
         with open(file) as f:
             terms = [l.upper().strip('\n').split() for l in f]
-        terms = [[term[0].split('_', 1)[-1][:30]]+term[1:] for term in terms if term]
+        if clean:
+            terms = [[term[0].split('_', 1)[-1][:30]]+term[1:] for term in terms if term]
         annot+=terms
 
     var_names = adata.var_names.str.upper()
     I = [[int(gene in term) for term in annot] for gene in var_names]
     I = np.asarray(I, dtype='int32')
-
 
     mask = I.sum(0) > min_genes
     if max_genes is not None:
