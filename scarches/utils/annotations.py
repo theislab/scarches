baee22a344fa14c03d49736d3385f6c6cc1ea75b
Koncopd <koncopd@gmail.com>
2021-07-28 15:04:59 +0200

    add genes_use_upper to add_annotations

--- scarches/utils/annotations.py
+++ scarches/utils/annotations.py
@@ -3,18 +3,20 @@ import numpy as np
 # add binary I of size n_vars x number of annotated terms in files
 # if I[i,j]=1 then gene i is active in annotation j
 def add_annotations(adata, files, min_genes=0, max_genes=None, varm_key='I', uns_key='terms',
-                    clean=True):
+                    clean=True, genes_use_upper=True):
     files = [files] if isinstance(files, str) else files
     annot = []
 
     for file in files:
         with open(file) as f:
-            terms = [l.upper().strip('\n').split() for l in f]
+            p_f = [l.upper() for l in f] if genes_use_upper else f
+            terms = [l.strip('\n').split() for l in p_f]
+
         if clean:
             terms = [[term[0].split('_', 1)[-1][:30]]+term[1:] for term in terms if term]
         annot+=terms
 
-    var_names = adata.var_names.str.upper()
+    var_names = adata.var_names.str.upper() if genes_use_upper else adata.var_names
     I = [[int(gene in term) for term in annot] for gene in var_names]
     I = np.asarray(I, dtype='int32')
 
