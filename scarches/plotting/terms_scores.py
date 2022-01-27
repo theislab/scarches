b52e4b890227cea75bdc58024601ea6f2e3bcdb7
Koncopd <koncopd@gmail.com>
2022-01-27 16:08:16 +0100

    fix docs

--- scarches/plotting/terms_scores.py
+++ scarches/plotting/terms_scores.py
@@ -5,7 +5,9 @@ from typing import Union
 
 def plot_abs_bfs_key(scores, terms, key, n_points=30, lim_val=2.3, fontsize=8, scale_y=2, yt_step=0.3,
                      title=None, ax=None):
-
+    """\
+    Plot the absolute bayes scores rankings.
+    """
     txt_args = dict(
         rotation='vertical',
         verticalalignment='bottom',
--- scarches/utils/__init__.py
+++ scarches/utils/__init__.py
@@ -1,1 +1,3 @@
 from .annotations import add_annotations
+
+__all__ = ('add_annotations',)
