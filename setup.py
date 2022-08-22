cfd2358cbe13e86a2c5fa93e1604f3db3dfa34c4
Koncopd <koncopd@gmail.com>
2022-08-22 17:45:11 +0200

    fix deprecation

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
     from scarches import __author__, __email__, __version__
 except ImportError:  # Deps not yet installed
     __author__ = __email__ = ''
-    __version__ = '0.5.3'
+    __version__ = '0.5.4'
 
 setup(name='scArches',
       version=__version__,
