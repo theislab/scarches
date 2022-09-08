615fcf236447176fac15cc15c0fc2858536ad0be
Elihei2 <eheidari@student.ethz.ch>
2022-09-08 18:14:29 +0200

    added datasets

--- scarches/models/sagenet/__init__.py
+++ scarches/models/sagenet/__init__.py
@@ -2,5 +2,5 @@ __author__ = __maintainer__ = "Elyas Heidari"
 __email__ = 'eheidari@student.ethz.ch'
 __version__ = "0.1.1"
 
-from . import classifier, model, sage, utils, MGA_data, DHH_data
+from . import classifier, model, sage, utils
 
