41bece208f63d88f50589800d90578a22f67c399
Elihei2 <eheidari@student.ethz.ch>
2022-09-08 17:32:54 +0200

    added datasets

--- scarches/dataset/__init__.py
+++ scarches/dataset/__init__.py
@@ -1,8 +1,8 @@
 from .trvae._utils import label_encoder
 from .trvae.data_handling import remove_sparsity
 from .trvae.anndata import AnnotatedDataset as trVAEDataset
-from . import DDH_data
-from . import MGA_data
+from .DDH_data import _DDH_data
+from .DDH_data import _MGA_data
 
 __all__ = ('label_encoder', 'remove_sparsity', 'trVAEDataset',
 	"scRNAseq",
