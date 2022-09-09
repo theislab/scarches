a7353854e71f4e030ab819832955a86d91463388
Elihei2 <eheidari@student.ethz.ch>
2022-09-09 09:22:31 +0200

    added datasets

--- scarches/models/__init__.py
+++ scarches/models/__init__.py
@@ -7,7 +7,7 @@ from .scgen.vaearith_model import scgen
 from .sagenet.sage import sage
 from .sagenet.utils import *
 # from .sagenet.DDH_data._DHH_data import *
-from .sagenet.MGA_data import *
+# from .sagenet.MGA_data import *
 from .expimap.expimap import expiMap
 from .expimap.expimap_model import EXPIMAP
 
