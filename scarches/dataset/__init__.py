16a272e7056ab40a064a11a1173eb58c084fb09d
Elihei2 <eheidari@student.ethz.ch>
2022-09-09 14:02:50 +0200

    added try-catch for pyG and captum

--- scarches/dataset/__init__.py
+++ scarches/dataset/__init__.py
@@ -1,7 +1,7 @@
 from .trvae._utils import label_encoder
 from .trvae.data_handling import remove_sparsity
 from .trvae.anndata import AnnotatedDataset as trVAEDataset
-from .DDH_data import *
+# from .DDH_data import *
 from .MGA_data import *
 
 __all__ = ('label_encoder', 'remove_sparsity', 'trVAEDataset'
