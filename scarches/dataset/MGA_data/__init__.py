cbd9a13d0b65eb8955373936479c033b3fcb755b
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2022-09-14 15:33:02 +0200

    fixed the import bug

--- scarches/dataset/MGA_data/__init__.py
+++ scarches/dataset/MGA_data/__init__.py
@@ -1,1 +1,5 @@
-from ._MGA_data import seqFISH1_1, seqFISH2_1, seqFISH3_1, seqFISH1_2, seqFISH2_2, seqFISH3_2, scRNAseq
+try:
+    from .MGA_data import seqFISH1_1, seqFISH2_1, seqFISH3_1, seqFISH1_2, seqFISH2_2, seqFISH3_2, scRNAseq
+except:
+    import warnings
+    warnings.warn('In order to use the mouse gastrulation seqFISH datsets, please install squidpy (see https://github.com/scverse/squidpy).')
