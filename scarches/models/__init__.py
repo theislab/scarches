1c7720a69dfd695c8798e54987f0f4867c514986
Koncopd <koncopd@gmail.com>
2022-09-14 14:44:44 +0200

    fix warnings on sagenet import

--- scarches/models/__init__.py
+++ scarches/models/__init__.py
@@ -4,10 +4,11 @@ from .trvae.adaptors import Adaptor, attach_adaptors
 from .scgen.vaearith import vaeArith
 from .scgen.vaearith_model import scgen
 try:
-  from .sagenet.sagenet import sagenet
-  from .sagenet.utils import glasso
+    from .sagenet.sagenet import sagenet
+    from .sagenet.utils import glasso
 except:
-  warnings.warn('In order to use sagenet models, please install pytorch geometric (see https://pytorch-geometric.readthedocs.io) and \n captum (see https://github.com/pytorch/captum).')
+    import warnings
+    warnings.warn('In order to use sagenet models, please install pytorch geometric (see https://pytorch-geometric.readthedocs.io) and \n captum (see https://github.com/pytorch/captum).')
 from .expimap.expimap import expiMap
 from .expimap.expimap_model import EXPIMAP
 from scvi.model import SCVI, SCANVI, TOTALVI
