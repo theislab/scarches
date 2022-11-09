1dc8e30cb03058ae3f38ff0d39bbb2bf26a31830
M0hammadL <33202701+M0hammadL@users.noreply.github.com>
2022-11-09 08:23:53 +0100

    Update __init__.py

--- scarches/__init__.py
+++ scarches/__init__.py
@@ -1,15 +1,13 @@
 from . import dataset, metrics, trainers, models, zenodo, plotting, utils, classifiers
 
 __author__ = ', '.join([
+    'Mohammad Lotfollahi',
+    'Sergei Rybakov',
     'Marco Wagenstetter',
     'Mohsen Naghipourfar',
-    'Mohammad Lotfollahi',
-    'Sergei Rybakov'
 ])
 
 __email__ = ', '.join([
-    'marco.wagenstetter@tum.de',
-    'naghipourfar@ce.sharif.edu',
     'mohammad.lotfollahi@helmholtz-muenchen.de',
     'sergei.rybakov@helmholtz-muenchen.de'
 ])
