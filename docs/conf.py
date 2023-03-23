44743a1b1830dc70b05ef54e77dd52ded518607e
Koncopd <koncopd@gmail.com>
2023-03-23 14:54:49 +0100

    change docs theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -18,13 +18,9 @@ from datetime import datetime
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Readthedocs theme -------------------------------------------------------
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "furo"
 
 import scarches
 
