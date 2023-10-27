ffdccdd86849da531d011829c95dce9693f0e11c
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2023-10-27 16:40:24 +0200

    Improve train/test split performance when cell_type is available

--- scarches/trainers/scpoli/_utils.py
+++ scarches/trainers/scpoli/_utils.py
@@ -127,28 +127,26 @@ def train_test_split(adata, train_frac=0.85, condition_keys=None, cell_type_key=
         labeled_idx = indices[labeled_array == 1]
         unlabeled_idx = indices[labeled_array == 0]
 
-        train_labeled_idx = []
-        val_labeled_idx = []
-        train_unlabeled_idx = []
-        val_unlabeled_idx = []
+        train_labeled_idx = np.array([], dtype=int)
+        val_labeled_idx = np.array([], dtype=int)
+        train_unlabeled_idx = np.array([], dtype=int)
+        val_unlabeled_idx = np.array([], dtype=int)
 
         if len(labeled_idx) > 0:
-            cell_types = adata[labeled_idx].obs[cell_type_key].unique().tolist()
-            for cell_type in cell_types:
-                ct_idx = labeled_idx[adata[labeled_idx].obs[cell_type_key] == cell_type]
-                n_train_samples = int(np.ceil(train_frac * len(ct_idx)))
-                np.random.shuffle(ct_idx)
-                train_labeled_idx.append(ct_idx[:n_train_samples])
-                val_labeled_idx.append(ct_idx[n_train_samples:])
+            cell_type_info = adata[labeled_idx].obs[[cell_type_key]].copy()
+            cell_type_info['random'] = np.random.rand(len(cell_type_info.index))
+            cell_type_info['count_in_ct'] = cell_type_info.groupby(cell_type_key, observed=True)['random'].transform('count')
+            cell_type_info['rank_in_ct'] = cell_type_info.groupby(cell_type_key, observed=True)['random'].rank(method="first") - 1
+            cell_type_info['train'] = cell_type_info['count_in_ct'] * train_frac > cell_type_info['rank_in_ct']
+            train_labeled_idx = labeled_idx[cell_type_info['train']]
+            val_labeled_idx = labeled_idx[~cell_type_info['train']]
         if len(unlabeled_idx) > 0:
             n_train_samples = int(np.ceil(train_frac * len(unlabeled_idx)))
-            train_unlabeled_idx.append(unlabeled_idx[:n_train_samples])
-            val_unlabeled_idx.append(unlabeled_idx[n_train_samples:])
-        train_idx = train_labeled_idx + train_unlabeled_idx
-        val_idx = val_labeled_idx + val_unlabeled_idx
+            train_unlabeled_idx = unlabeled_idx[:n_train_samples]
+            val_unlabeled_idx = unlabeled_idx[n_train_samples:]
 
-        train_idx = np.concatenate(train_idx)
-        val_idx = np.concatenate(val_idx)
+        train_idx = np.concatenate([train_labeled_idx, train_unlabeled_idx])
+        val_idx = np.concatenate([val_labeled_idx, val_unlabeled_idx])
 
     elif condition_keys is not None:
         train_idx = []
